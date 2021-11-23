0ef41b644d617d0c41f47ac8f933e2a4e4859106
Ontlametse31281060 <92276676+Ontlametse31281060@users.noreply.github.com>
2021-11-23 07:40:44 +0200

    Add files via upload

--- ImageUploadDelete/Controllers/ImageController.cs
+++ ImageUploadDelete/Controllers/ImageController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using ImageUploadDelete.Models;
 using System.IO;
 using Microsoft.AspNetCore.Http;
